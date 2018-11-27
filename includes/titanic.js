0742c26ab61eaf680f8aec83844656eedcbbe5af
salathiel2000 <salathiel2000@gmail.com>
2018-11-27 09:44:26 -0800

    Changed Location of my repository
    Changed to a new version of XAMPP to allow for secure webpages to load, hence I had to move my repo to a new local folder

--- includes/titanic.js
+++ includes/titanic.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
 
+    var filterDeck; 
+    var myUrl = "deckFilter.php?search="+search+"filterDeck="+filterDeck
+
     $("#filterDeck").change(function(event){
         event.preventDefault(); 
-        var filterDeck = $(this).val(); 
-        var myUrl = "deckFilter.php?serch=&filterDeck="+filterDeck;
+        filterDeck = $(this).val(); 
+        // var myUrl = "deckFilter.php?serch=&filterDeck="+filterDeck;
          $.get(myUrl, function(data, status){
             if(status == "success"){
                 $("#result").html(data); 
-                console.log(data);  
-                
+                console.log(data);      
             }
          });
     });
