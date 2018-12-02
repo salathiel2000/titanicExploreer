f25b2922dc0879791c520894635f9ef2cee63216
salathiel2000 <salathiel2000@gmail.com>
2018-12-01 22:11:14 -0800

    Pagination linked to AJAX (not fully implemented)

--- includes/titanic.js
+++ includes/titanic.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     var search = $("#search").val(); 
     var filterDeck = $("#filterDeck").val(); 
     var filterClass = $("#filterClass").val(); 
+    var pageNum = 27;  
     // var myUrl = "deckFilter.php?search="+search+"filterDeck="+filterDeck
 
     $("tbody").attr("id", "result"); 
@@ -13,10 +14,13 @@ $(document).ready(function(){
         filterDeck = $(this).val(); 
         search = $("#search").val(); 
         filterClass = $("#filterClass").val(); 
-        var myUrl = "deckFilter.php?search="+search+"&filterDeck="+filterDeck+"&filterClass="+filterClass;
+        pageNum = parseInt($("#pages").text());
+        var myUrl = "deckFilter.php?search="+search+"&filterDeck="+filterDeck+"&filterClass="+filterClass+"&pageNum="+pageNum;
          $.get(myUrl, function(data, status){
             if(status == "success"){
-                $("#result").html(data); 
+                var res = $.parseJSON(data); 
+                $("#result").html(res['output']); 
+                $("#pages").html(res['pages']);
                 // $("#testCode").html(data);  //update query with AJAX for debugging purpose
                 console.log(myUrl);     
             }
@@ -29,10 +33,13 @@ $(document).ready(function(){
         filterClass = $(this).val(); 
         search = $("#search").val(); 
         filterDeck = $("#filterDeck").val();  
-        var myUrl = "deckFilter.php?search="+search+"&filterDeck="+filterDeck+"&filterClass="+filterClass;
+        pageNum = parseInt($("#pages").text());
+        var myUrl = "deckFilter.php?search="+search+"&filterDeck="+filterDeck+"&filterClass="+filterClass+"&pageNum="+pageNum;
         $.get(myUrl, function(data, status){
             if(status == "success"){
-                $("#result").html(data); 
+                var res = $.parseJSON(data); 
+                $("#result").html(res['output']);
+                $("#pages").html(res['pages']);
                 // $(".#testCode").html(data); //update query with AJAX for debugging purpose
                 console.log(myUrl);       
             }
@@ -45,10 +52,13 @@ $(document).ready(function(){
         search = $(this).val(); 
         filterDeck = $("#filterDeck").val(); 
         filterClass = $("#filterClass").val();   
-        var myUrl = "deckFilter.php?search="+search+"&filterDeck="+filterDeck+"&filterClass="+filterClass;
+        pageNum = parseInt($("#pages").text());
+        var myUrl = "deckFilter.php?search="+search+"&filterDeck="+filterDeck+"&filterClass="+filterClass+"&pageNum="+pageNum;
          $.get(myUrl, function(data, status){
             if(status == "success"){
-                $("#result").html(data); 
+                var res = $.parseJSON(data); 
+                $("#result").html(res['output']);
+                $("#pages").html(res['pages']);
                 // $("#testCode").html(data); //update query with AJAX for debugging purpose
                 console.log("search:"+myUrl);      
             }
@@ -56,7 +66,3 @@ $(document).ready(function(){
     });
 
 });
-
-
-
-// });
